ce51c42c3a3c9906cde0d131fe21d1a4baed41be
Pierre Guillemot <pierregllmt@gmail.com>
2024-06-15 19:39:05 +0200

    add animation with gsap

--- src/slices/Integrations/index.tsx
+++ src/slices/Integrations/index.tsx
@@ -7,21 +7,10 @@ import {
 } from "@prismicio/react";
 import StarBackground from "./StarBackground";
 import Image from "next/image";
-
-import {
-  FaDigitalOcean,
-  FaCloudflare,
-  FaNpm,
-  FaGithub,
-  FaFigma,
-  FaFly,
-} from "react-icons/fa6";
-
-import StylizedLogoMark from "./StylizedLogoMark";
-
 import background from "./background.jpg";
 import React from "react";
 import clsx from "clsx";
+import AnimatedContent from "./AnimatedContent";
 
 /**
  * Props for `Integrations`.
@@ -32,15 +21,6 @@ export type IntegrationsProps = SliceComponentProps<Content.IntegrationsSlice>;
  * Component for "Integrations" Slices.
  */
 const Integrations = ({ slice }: IntegrationsProps): JSX.Element => {
-  const icons: { [key: string]: JSX.Element } = {
-    digitalocean: <FaDigitalOcean />,
-    cloudflare: <FaCloudflare />,
-    npm: <FaNpm />,
-    github: <FaGithub />,
-    figma: <FaFigma />,
-    fly: <FaFly />,
-  };
-
   return (
     <Bounded
       data-slice-type={slice.slice_type}
@@ -54,6 +34,7 @@ const Integrations = ({ slice }: IntegrationsProps): JSX.Element => {
         className="object-cover"
         quality={90}
       />
+      <StarBackground />
 
       <div className="relative">
         <h2 className="mx-auto max-w-2xl text-balance text-center text-5xl font-medium md:text-7xl">
@@ -63,42 +44,10 @@ const Integrations = ({ slice }: IntegrationsProps): JSX.Element => {
         <div className="mx-auto mt-6 max-w-md text-balance text-center text-slate-300">
           <PrismicRichText field={slice.primary.body} />
         </div>
+        <AnimatedContent slice={slice} />
       </div>
 
-      <div className="mt-20 flex flex-col items-center md:flex-row">
-        {slice.primary.icons.map((item, index) => (
-          <React.Fragment key={index}>
-            {index === Math.floor(slice.primary.icons.length / 2) && (
-              <>
-                <StylizedLogoMark />
-                <div className="signal-line rotate-180 bg-gradient-to-t" />
-              </>
-            )}
-            <div className="pulsing-icon flex aspect-square shrink-0 items-center justify-center rounded-full border border-blue-50/30 bg-blue-50/25 p-3 text-3xl text-blue-100 opacity-40 md:text-4xl lg:text-5xl">
-              {item.icon && icons[item.icon]}
-            </div>
-            {index !== slice.primary.icons.length - 1 && (
-              <div
-                className={clsx(
-                  "signal-line",
-                  index >= Math.floor(slice.items.length / 2)
-                    ? "rotate-180"
-                    : "rotate-0",
-                )}
-              />
-            )}
-          </React.Fragment>
-        ))}
-      </div>
-
-      {/* <>
-        {slice.primary.icons.map((item) => {
-          // Render the item
-          <>{item.icon}</>;
-        })}
-      </> */}
-
-      <StarBackground />
+      {/* <StarBackground /> */}
     </Bounded>
   );
 };
